edae42161d5d4b04d2800417e2585c739bc2bc8e
Etin Obaseki <etinosa.obaseki@supermartng.com>
2019-10-02 23:01:03 +0100

    Set up settings

---
+++ database/seeds/SettingsTableSeeder.php
@@ -0,0 +1,118 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SettingsTableSeeder extends Seeder
+{
+    /**
+     * @var array
+     */
+    protected $settings = [
+        [
+            'key'                       =>  'site_name',
+            'value'                     =>  'Swaggie Wears',
+        ],
+        [
+            'key'                       =>  'site_title',
+            'value'                     =>  'Online Store Swaggie Wears Co.',
+        ],
+        [
+            'key'                       =>  'default_email_address',
+            'value'                     =>  'swaggiewears@gmail.com',
+        ],
+        [
+            'key'                       =>  'currency_code',
+            'value'                     =>  'NGN',
+        ],
+        [
+            'key'                       =>  'currency_symbol',
+            'value'                     =>  '₦',
+        ],
+        [
+            'key'                       =>  'site_logo',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'site_favicon',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'footer_copyright_text',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'seo_meta_title',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'seo_meta_description',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'social_facebook',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'social_twitter',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'social_instagram',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'social_linkedin',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'google_analytics',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'facebook_pixels',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'paystack_payment_method',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'paystack_key',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'paystack_secret_key',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'paypal_payment_method',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'paypal_client_id',
+            'value'                     =>  '',
+        ],
+        [
+            'key'                       =>  'paypal_secret_id',
+            'value'                     =>  '',
+        ],
+    ];
+    
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach ($this->settings as $index => $setting)
+        {
+            $result = Setting::create($setting);
+            if (!$result) {
+                $this->command->info("Insert failed at record $index.");
+                return;
+            }
+        }
+        $this->command->info('Inserted '.count($this->settings). ' records');
+    }
+}
