78dafdf2e6f905ae8a64c5ad599d50358ddabe94
Etin Obaseki <etinosa.obaseki@supermartng.com>
2019-10-02 23:02:48 +0100

    Add Attributes

---
+++ app/Models/Attribute.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attribute extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'attributes';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'code', 'name', 'frontend_type', 'is_filterable', 'is_required'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts  = [
+        'is_filterable' =>  'boolean',
+        'is_required'   =>  'boolean',
+    ];
+}
