96ad7265cdf81287e9de2b37847b5f22ef7e7a80
Etin Obaseki <etinosa.obaseki@supermartng.com>
2019-10-11 21:58:30 +0100

    Add brands

---
+++ app/Http/Controllers/Admin/BrandController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Contracts\BrandContract;
+use App\Http\Controllers\BaseController;
+
+class BrandController extends BaseController
+{
+    //
+}
